5d8b6d7fd63e110835fea92eb2a8f7de6653e23e
NihadHub <ouachournihad@gmail.com>
2026-08-17 22:38:48 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/signix/SignixApplication.java
@@ -0,0 +1,13 @@
+package com.signix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/signix/SignixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
